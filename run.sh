5bb68645abea2ed116df8b8fcbeae1dcf414deaa
Michael Brown <mikeabrown2020@gmail.com>
2021-09-01 22:45:59 -0400

    09012021 updates

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 cd /home/michael/script
+today=`date +%m%d%Y`
+mkdir data/$today
 python3 StateData.py
 python3 USA.py
+mv *.png data/$today
+mv *.csv data/$today
 git add *
+git commit -m "`date +%m%d%Y` updates"
 
